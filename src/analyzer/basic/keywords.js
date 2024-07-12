c1026057b4d8af1c071b86b2d24e164d0e4c1850
Bora Sevim <sevimbora@gmail.com>
2024-07-12 13:50:38 +0300

    title and description keyword analyze added

--- src/analyzer/basic/keywords.js
+++ src/analyzer/basic/keywords.js
@@ -6,7 +6,33 @@ What's more, it's pretty off-putting for potential readers, who are more likely 
 
 function CheckKeywords(dom)
 {
-    return "empty";
+    const keywords = dom.window.document.querySelectorAll("meta[name='keywords, Keywords']");
+    const htmlTitle = dom.window.document.title;
+    const metaDescription = dom.window.document.querySelector("meta[name='description']");
+    var titleHasKeyword, descriptionHasKeyword;
+    if(htmlTitle !== null && metaDescription !== null)
+    {
+        const descriptionContent = metaDescription.getAttribute("content");
+        for (var i = 0; i < descriptionContent.length; i++) {
+            for(var j = 0; j < keywords.length; j++)
+            {
+                if(descriptionContent[i] === keywords[j]){descriptionHasKeyword = true;}
+            }
+        }
+
+        for (var i = 0; i < htmlTitle.length; i++) {
+            for(var j = 0; j < keywords.length; j++)
+            {
+                if(htmlTitle[i] === keywords[j]){titleHasKeyword = true;}
+            }
+        }
+
+        if(!descriptionHasKeyword && titleHasKeyword){return "negative - description does not include keywords";}
+        if(descriptionHasKeyword && !titleHasKeyword){return "negative - title does not include keywords";}
+        if(!descriptionHasKeyword && !titleHasKeyword){return "negative - neither description nor title includes keywords";}
+        else{return "positive";}
+    }
+    return "negative - no keywords found";
 }
 
 export default CheckKeywords;

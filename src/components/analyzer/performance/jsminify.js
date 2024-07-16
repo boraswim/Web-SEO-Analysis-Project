16c9cc5b250dfb75fdc0672b662ae845deb45889
Bora Sevim <sevimbora@gmail.com>
2024-07-16 11:23:02 +0300

    npm run server fixed, jsminify analyze implemented

--- src/components/analyzer/performance/jsminify.js
+++ src/components/analyzer/performance/jsminify.js
@@ -8,7 +8,13 @@ There are server-side tools (including WordPress plugins) to automatically minif
 
 function CheckJsMinify(dom)
 {
-    return "empty";
+    const scripts = dom.window.document.scripts;
+    for(var i = 0; i < scripts.length; i++)
+    {
+        if(scripts[i].innerHTML.includes(' '))
+            return "negative - there are unminified js files"
+    }
+    return "positive";
 }
 
 export default CheckJsMinify;

d05146a409fce94f4eb01389f01779342c70f7ab
Bora Sevim <sevimbora@gmail.com>
2024-07-16 11:42:06 +0300

    cssminify analyze implemented

--- src/components/analyzer/performance/cssminify.js
+++ src/components/analyzer/performance/cssminify.js
@@ -8,7 +8,24 @@ There are server-side tools (including WordPress plugins) to automatically minif
 
 function CheckCssMinify(dom)
 {
-    return "empty";
+    const inlineStyles = dom.window.document.querySelectorAll('[style]');
+    const internalStyles = dom.window.document.querySelectorAll('style');
+    for(var i = 0; i < inlineStyles.length; i++)
+    {
+        if(inlineStyles[i].getAttribute('style').includes(' '))
+        {
+            return "negative - there are unminified css files";
+        }
+    }
+    
+    for(var i = 0; i < internalStyles.length; i++)
+    {
+        if(internalStyles[i].innerHtml.includes(' '))
+        {
+            return "negative - there are unminified css files";
+        }
+    }
+    return "positive";
 }
 
 export default CheckCssMinify;

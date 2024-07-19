444c524682251b3030991be84f00d6d1a3ef9405
Bora Sevim <sevimbora@gmail.com>
2024-07-19 17:21:14 +0300

    created object for noindex analyze result

--- src/components/analyzer/advanced/noindex.js
+++ src/components/analyzer/advanced/noindex.js
@@ -4,11 +4,28 @@ Only ever use noindex meta tag or header on pages you want to keep out of the re
 
 function CheckNoIndexHeader(dom)
 {
+    const checkNoIndexObj = {};
+    const checkNoIndexArray = [];
     const noIndexElements = dom.window.document.querySelectorAll("meta[content='noindex']");
     
-    if(noIndexElements.length > 0){return "negative - noindex meta content found";}
-
-    return 'positive';
+    if(noIndexElements.length > 0)
+    {
+        checkNoIndexObj['status'] = 'negative';
+        checkNoIndexObj['description'] = 'noindex meta tags found';
+        for(let i = 0; i < noIndexElements.length; i++)
+        {
+            checkNoIndexArray[noIndexElements.length] = noIndexElements[i];
+        }
+    }
+    
+    else
+    {
+        checkNoIndexObj['status'] = 'positive';
+        checkNoIndexObj['description'] = 'noindex meta tags not found';
+    }
+    
+    checkNoIndexObj["instances"] = checkNoIndexArray;
+    return checkNoIndexObj;
 }
 
 export default CheckNoIndexHeader;
